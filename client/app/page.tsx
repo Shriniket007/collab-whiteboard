98dc4b9b6bc802d4ce79400530c1d81c3693fc51
shriniket <shriniketkulkarni42@gmail.com>
2023-12-26 14:03:01 +0530

    #

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,7 +6,7 @@ import { ChromePicker } from 'react-color'
 
 import { io } from 'socket.io-client'
 import { drawLine } from '../utils/drawLine'
-const socket = io('http://localhost:3001')
+const socket = io('https://white-board-backend-v0yh.onrender.com')
 
 interface pageProps {}
 
